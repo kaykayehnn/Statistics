385bff1606057e7197f2d9ab996329ba89c58e20
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-11-28 22:58:46 +0200

    Fixed minor bug with route order.

--- server/config/express.js
+++ server/config/express.js
@@ -4,5 +4,4 @@ const handlebars = require('express-handlebars')
 module.exports = (app, config) => {
   app.set('view engine', 'handlebars')
   app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-  app.use(express.static('public'))
 }
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,15 @@
+const express = require('express')
 let handlers = require('../handlers/index')
 
 module.exports = (app, config) => {
   handlers = handlers(config)
 
-  app.get(/^\/(hour|day|week|)$/i, handlers.homePage)
+  app.get('/', (req, res) => res.redirect('/hour'))
+  app.get(/^\/(hour|day|week)$/i, handlers.homePage)
   app.get('/peaks', handlers.peaks)
   app.get('/data', handlers.data)
   app.get('/index*', (req, res) => res.redirect('/hour'))
 
+  app.use(express.static('public'))
   app.all('*', handlers.notFound)
 }
