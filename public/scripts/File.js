802aa933f095f230bd253015d5865c8e4fcf35d9
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-11-30 20:53:26 +0200

    Added average line to statistics. May not make it to master.

--- public/scripts/File.js
+++ public/scripts/File.js
@@ -3,27 +3,27 @@ $(document).ready(function () {
   google.charts.load('current', { 'packages': ['corechart'] })
   google.charts.setOnLoadCallback(getData)
 
-  function getData() {
+  function getData () {
     var address = document.location.href.split(window.location.host)[1]
     period = address.slice(1) || 'hour'
     $.ajax({
       url: 'data' + '?period=' + period
     }).then(formatData)
-    .then(drawChart)
+      .then(drawChart)
   }
 
-  function formatData(data) {
+  function formatData (data) {
     for (var i = 0; i < data.length; i++) {
       data[i] = [new Date(data[i].date), data[i].avg]
     }
     data.splice(0, 0, ['Time', 'Usage'])
-    $(window).resize(function() {
+    $(window).resize(function () {
       drawChart(data)
     })
     return data
   }
 
-  function drawChart(data) {
+  function drawChart (data) {
     data = google.visualization.arrayToDataTable(data)
     var options = {
       chartArea: {
@@ -31,6 +31,13 @@ $(document).ready(function () {
         height: '80%'
       },
       title: 'CPU usage during last ' + period,
+      trendlines: {
+        0: {
+          type: 'polynomial',
+          tooltip: false,
+          degree: 3
+        }
+      },
       legend: {
         position: 'bottom'
       }
