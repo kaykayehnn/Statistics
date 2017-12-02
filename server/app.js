f1a0b59d43d779c4fbb66582ac5fa6a66ca9a1f0
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-12-02 17:11:16 +0200

    Merged app and logging logic in one file so extensibility would be easier.

--- server/app.js
+++ server/app.js
@@ -2,10 +2,14 @@ let env = 'development'
 const app = require('express')()
 
 const config = require('./config/settings')(env)
+const ee = require('./logging/eventEmitter')
 require('./config/db')(config)
+require('./logging/dataCollector')(config, ee)
+require('./logging/commandLine')(config, ee)
 require('./config/express')(app, config)
 require('./config/routes')(app, config)
 
+console.log('Database running')
 app.listen(config.port)
 console.log('Server running')
 
--- server/logger.js
+++
@@ -1,9 +0,0 @@
-let env = 'development'
-
-const config = require('./config/settings')(env)
-const ee = require('./logging/eventEmitter')
-require('./config/db')(config)
-require('./logging/dataCollector')(config, ee)
-require('./logging/commandLine')(config, ee)
-
-console.log('Database running')
