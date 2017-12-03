23c8bdf18b3c4f0749ee14370476633866a1d10a
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-12-03 09:55:27 +0200

    Removed db logging to console in produciton environment.

--- server/logging/dataCollector.js
+++ server/logging/dataCollector.js
@@ -1,17 +1,19 @@
 const Entry = require('../models/Entry')
 let cl = require('./commandLine')
 
-let addToDB = (data) => {
-  data = { cpu: data }
-  let entry = { date: Date.now(), data }
-  Entry
-    .create(entry)
-    .then(console.log)
-    .catch((err) => console.log(err))
-}
-
 module.exports = (ee, config) => {
   cl = cl(ee, config)
+
+  let addToDB = (data) => {
+    data = { cpu: data }
+    let entry = { date: Date.now(), data }
+    Entry
+      .create(entry)
+      .then(data => {
+        if (config.env !== 'production') console.log(data)
+      })
+      .catch((err) => console.log(err))
+  }
 
   ee.addListener('data', addToDB)
 
