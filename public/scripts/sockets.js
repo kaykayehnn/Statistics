dc9afd4b54c95c75c2f921ced060d0056c21ab6c
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-12-03 09:05:10 +0200

    Minor improvement to notification appearance.

--- public/scripts/sockets.js
+++ public/scripts/sockets.js
@@ -1,20 +1,17 @@
-var socket = io()
-socket.on('peak', (data) => {
-  var currentAlerts = $('.alert-danger')
-  var element = $('<div class="alert alert-danger" style="display:none"></div>').html('<strong>High CPU Usage! ' + data + '%</strong>')
-  console.log(currentAlerts.length)
-  if (currentAlerts.length >= 3) {
-    currentAlerts.last().fadeOut()
-  }
-  element.prependTo($('#alerts')).fadeIn()
-  element.click(e => $(e.target).fadeOut())
+$(document).ready(function () {
+  var socket = io()
+  socket.on('peak', (data) => {
+    var currentAlerts = $('.alert-danger:visible')
+    var element = $('<div class="alert alert-danger" style="display:none"></div>')
+      .html('<strong>High CPU Usage! ' + data + '%</strong>')
+      .click(function (e) { $(e.target).fadeOut(removeHidden) })
+    var removeHidden = function () { $('.alert-danger:hidden').remove() }
+    var add = function () { element.prependTo($('#alerts')).fadeIn() }
+    console.log(currentAlerts.length)
+    if (currentAlerts.length >= 3) {
+      currentAlerts.last().fadeOut(function () { removeHidden(); add() })
+    } else {
+      add()
+    }
+  })
 })
-
-// $('form').submit((e) => {
-//   socket.emit('chat message', $('#m').val())
-//   $('#m').val('')
-//   e.preventDefault()
-// })
-// socket.on('chat message2', msg => {
-//   $('#messages').append($('<li>').text(msg))
-// })
