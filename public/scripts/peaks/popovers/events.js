adcc26762aae2416f7adbe2225df7621788373d8
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-12-09 12:14:42 +0200

    Now Safari should work.

--- public/scripts/peaks/popovers/events.js
+++ public/scripts/peaks/popovers/events.js
@@ -3,18 +3,25 @@ define(['peaks/popovers/actions'], function (actions) {
     target
       .mousedown(function (e) {
         e.stopImmediatePropagation()
-        hideOther()
+        hideOther(e.target)
         actions(e.target).toggle()
       })
       .focus(function (e) {
+        hideOther(e.target)
         actions(e.target).show()
       })
       .blur(function (e) {
         actions(e.target).hide()
       })
 
-    function hideOther () {
-      actions('.hasPopover').hide()
+    function hideOther (target) {
+      var all = $('.hasPopover')
+      for (var i = 0; i < all.length; i++) {
+        var current = all[i]
+        if (current !== target) {
+          actions(current).hide()
+        }
+      }
     }
   }
 })
