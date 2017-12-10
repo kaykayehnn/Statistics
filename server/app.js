991d329bf31fe66bccf0c69c7683ac71402fb8de
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-12-10 12:26:52 +0200

    v1.0.0 Removed unnecessary module. Seems working well enough for a new major version.

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ require('./logging/dataCollector')(ee, config)
 require('./config/express')(app, config)
 require('./config/routes')(app, config)
 require('./config/sockets')(http, ee, config)
-require('./statistics/basic')(ee, config)
 
 http.listen(config.port, () => {
   console.log(`Server running on ${config.port}`)
--- server/statistics/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = (ee, config) => {
-  const basic = require('./basic')(ee, config)
-  const custom = require('./custom')
-  return [basic, custom]
-}
